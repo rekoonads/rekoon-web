1ffd50f28dc9e8049b20f4e04fadd366bd775fea
Kunal_Mukherjee <kunalmukherjee12@gmail.com>
2024-07-24 16:33:34 +0530

    Added Icon

--- src/components/Forms/DatePicker/DatePickerOne.tsx
+++ src/components/Forms/DatePicker/DatePickerOne.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/themes/material_green.css'; // Import your flatpickr theme
+import { SlCalender } from "react-icons/sl";
 
 interface DatePickerOneProps {
   onDateSelect?: (date: Date | null) => void;
+  text?: string
 }
 
-const DatePickerOne = ({ onDateSelect }: DatePickerOneProps) => {
+const DatePickerOne = ({ onDateSelect,text }: DatePickerOneProps) => {
   useEffect(() => {
     // Initialize flatpickr
     const picker = flatpickr('.form-datepicker', {
@@ -34,14 +36,19 @@ const DatePickerOne = ({ onDateSelect }: DatePickerOneProps) => {
   }, [onDateSelect]);
 
   return (
-    <div>
-      <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-        Date picker
+    <div className='flex flex-col text-center justify-center'>
+      <label className="relative right-15 mx-auto mb-3 block text-sm font-medium text-black dark:text-white">
+        {text}
       </label>
-      <input
+      <div className='flex gap-2 items-center w-[80%] border-[1.5px] border-stroke rounded-lg py-3 px-5'>
+      <SlCalender className='form-datepicker text-[23px]' />
+      <input type="text" className='form-datepicker w-full bg-transparent ml-1' />
+      {/* <input
         type="text"
-        className="form-datepicker w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-      />
+        className="form-datepicker w-[50%] rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+      /> */}
+      </div>
+      
     </div>
   );
 };
