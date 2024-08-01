58a8c25ac9fdf76bd4cde1c7982e84f51cea199f
shibu-g <shibugoper1@gmail.com>
2024-08-01 10:30:15 +0530

    createagency

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import advertiserCreation from "../controller/advertiserCreation.js";
+import createAgency from "../controller/createAgency.js";
 const router = Router();
 
 router.post("/addAdvertiser", advertiserCreation);
-
+router.post("/addAgency", createAgency);
 export default router
